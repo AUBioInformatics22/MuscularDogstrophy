61b31e75ebbd3d5d64c7a5b9d127b480d35e3904
Cassidy Schneider <71412336+cns0045@users.noreply.github.com>
2022-04-26 20:13:05 -0500

    Update create_figures.R

--- scripts/create_figures.R
+++ scripts/create_figures.R
@@ -3,28 +3,30 @@ library(knitr)
 
 setwd("~/GitHub/MuscularDogstrophy")
 
-#v_colors = viridis(4)
-
+# load data from csv (generated from stat_csv.sh)
 metrics_data=read.csv(file = "data/csv/all_metrics.csv", header = TRUE,
                       colClasses = c("factor", "numeric", "numeric", "numeric",
                       "numeric", "numeric", "numeric", "numeric", "numeric"))
 
+# define text for coverage legends
 legend_text <- c("Raw Whole Genome",
                  "Estimated Raw Chromosome X",
                  "Aligned Chromosome X",
                  "Marked Chromosome X",
                  "Variants Chromosome X")
 
+# define text for percent mapped legend
 pm_legend_text <- c("Whole Genome",
                     "X Chromosome")
 
+# define max y values for coverage plots
 max_value <- max(metrics_data[c("raw", "est_raw_chrX", "aligned_chrX",
                                 "marked_chrX")], na.rm = TRUE)
 max_value_all <- max(metrics_data[c("raw", "est_raw_chrX", "aligned_chrX",
                                 "marked_chrX", "variant_chrX")], na.rm = TRUE)
 
 
-
+# define coordinates for labels depending on how many categories are being graphed
 label_coords1 <- barplot(raw ~ id, data = metrics_data, space = 0.5)
 label_coords2 <- barplot(cbind(raw, aligned_chrX) ~ id, data = metrics_data,
                          beside = TRUE, space = c(0,0.5))  
@@ -33,16 +35,9 @@ label_coords3 <- barplot(cbind(raw, aligned_chrX, marked_chrX) ~ id,
 label_coords4 <- barplot(cbind(raw, aligned_chrX, marked_chrX, variant_chrX) ~ id,
                          data = metrics_data, beside = TRUE, space = c(0,0.5))
 
-#pd_labels <- barplot(percent_duplicates ~ id, data = metrics_data, ylim = c(0,100))
-
-#label_coord <- barplot(raw ~ id, data = metrics_data, space = 0.5)
-
-
-
-#pm_labels <- barplot(cbind(percent_mapped_wg, percent_mapped_chrX) ~ id,
-#                     data = metrics_data, space = c(0,0.5), beside = TRUE)
-
 ##### TABLES
+# generate markdown tables of data to accompany plots
+# Note: Concatenates new tables to existing ones.
 
 #tables <- file("analysis/0_figures/data_tables.md", open = "at")
 #cat(c("__Coverage__", kable(metrics_data[,c("id", "raw")],
@@ -67,6 +62,8 @@ label_coords4 <- barplot(cbind(raw, aligned_chrX, marked_chrX, variant_chrX) ~ i
 #    file = tables, sep = "\n")
 #close(tables)
 
+##### COVERAGE PLOTS
+# generate coverage plots for each step
 
 ## RAW
 png(filename="analysis/0_figures/1_coverage.png", width = 700, height = 400)
@@ -82,20 +79,6 @@ barplot(
   col = viridis(5)[4])
 text(x = label_coords1, y = metrics_data$raw + 1.5, labels = metrics_data$raw)
 dev.off()
-
-#png(filename="analysis/0_figures/1_coverage_all.png", width = 500, height = 300)
-#barplot(
-#  raw ~ rownames(coverage_data),
-#  data = coverage_data,
-#  space = 0.5,
-#  beside = TRUE,
-#  ylim = c(0,max_value*1.3),
-#  xlab = "Sequence",
-#  ylab = "Coverage",
-#  main = "Whole Genome Coverage of Raw Sequences",
-#  col = v_colors[3])
-#dev.off()
-
 
 ## RAW VS ALIGNED
 png(filename="analysis/0_figures/2_coverage.png", width = 700, height = 400)
@@ -141,8 +124,9 @@ text(x = t(label_coords3), y = cbind(metrics_data$raw,
                     round(metrics_data$marked_chrX, digits = 2)), cex = 0.8)
 dev.off()
 
+## RAW VS ALIGNED VS MARKED VS VARIANTS
 png(filename="analysis/0_figures/4_coverage.png", width = 700, height = 400)
-barplot(cbind(raw, aligned_chrX, marked_chrX, variant_chrX) ~ id,
+barplot(cbind(raw, aligned_chrX, marked_chrX, var_SNP, var_INDEL) ~ id,
         data = metrics_data,
         space = c(0,0.5),
         beside = TRUE,
@@ -152,20 +136,25 @@ barplot(cbind(raw, aligned_chrX, marked_chrX, variant_chrX) ~ id,
         main = "Coverage at Different Stages of Processing",
         legend.text = FALSE,
         #args.legend = ,
-        col = viridis(5)[4:1])
-legend("topright", inset = c(0,-0.1), legend = legend_text[c(1,3,4,5)],
-       cex = 0.8, fill = viridis(5)[4:1], bty = "n", xpd = TRUE)
-text(x = t(label_coords4), y = cbind(metrics_data$raw,
+        col = viridis(6)[5:1])
+legend("topright", inset = c(0,-0.07), legend = legend_text[c(1,3,4,5,6)],
+       cex = 0.8, fill = viridis(6)[5:1], bty = "n", xpd = TRUE)
+text(x = t(label_coords5), y = cbind(metrics_data$raw,
                                      metrics_data$aligned_chrX,
                                      metrics_data$marked_chrX,
-                                     metrics_data$variant_chrX) + 2,
+                                     metrics_data$var_SNP,
+                                     metrics_data$var_INDEL) + 2,
      labels = cbind(round(metrics_data$raw, digits = 2),
                     round(metrics_data$aligned_chrX,  digits = 2),
-                    round(metrics_data$marked_chrX, digits = 2)), cex = 0.8)
+                    round(metrics_data$marked_chrX, digits = 2),
+                    round(metrics_data$var_SNP, digits = 2),
+                    round(metrics_data$var_INDEL, digits = 2)), cex = 0.7)
 dev.off()
 
 
+
 ##### PERCENT PLOTS
+# generate plots of percentage metrics
 
 ## PERCENT MAPPED
 png(filename="analysis/0_figures/percent_mapped.png", width = 500, height = 300)
@@ -200,28 +189,3 @@ barplot(percent_duplicates ~ id,
 text(x = label_coords1, y = metrics_data$percent_duplicates + 1,
      labels = metrics_data$percent_duplicates, cex = 0.8)
 dev.off()
-
-
-
-##### HISTOGRAMS
-
-#png(filename="percent_mapped_plot.png", width = 500, height = 300)
-# hist(
-#   metrics_data$percent_mapped/100,
-#   #ylim = c(0,length(mapped_data$percent_mapped)),
-#   main = "Percent of Sequences Mapped in Samples",
-#   xlab = "Percent Mapped",
-#   ylab = "Frequency",
-#   col = viridis(5)[3])
-# #dev.off()
-#
-# #png(filename="percent_duplicates_plot.png", width = 500, height = 300)
-# #hist(
-# #  mapped_data$percent_mapped/100,
-# #ylim = c(0,length(mapped_data$percent_mapped)),
-# #  main = "Percent of Duplicate  in Samples",
-# #  xlab = "Percent Mapped",
-# #  ylab = "Frequency",
-# #  col = viridis(5)[3])
-# #dev.off()
-
