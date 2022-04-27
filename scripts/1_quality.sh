4ca43ebe5b0dfa64f66c34299b855bc65b4b9f46
Rebecca Nance <71400437+rln0005@users.noreply.github.com>
2022-04-27 10:23:51 -0500

    Update 1_quality.sh
    fixing script --becca

--- scripts/1_quality.sh
+++ scripts/1_quality.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+### This script uses FASTQC in parallel to generate quality reports for each sequence file. The output is a fastqc.html file for each sequence file that can be opened in a browser to view the graphical results.
+#### QUEUE PARAMETERS ####
+# class
+# 4 threads 
+# 2gb memory  
+# 12:00:00  
+
+#### MODULES #### 
 source /opt/asn/etc/asn-bash-profiles-special/modules.sh
 module load gnu_parallel
 module load fastqc
 
+#### DIRECTORIES #### 
 DATADIR=/home/aubclsb0103/MD_data/data/raw_data2/Springer_DMD
+
+#####################
+
+#move into directory where the sequence files are located
 cd $DATADIR
 
-#fastqc -t 8 *_2_*.fastq.gz
-#fastqc -t 2 H2LCVDSX3_s1_2_IDT8_UDI_024_i7-IDT8_UDI_024_i5_6797-RN-0001.fastq.gz H2LCVDSX3_s1_2_IDT8_UDI_027_i7-IDT8_UDI_027_i5_6797-RN-0004.fastq.gz
-
-parallel -j+0 --eta 'fastqc {}' ::: H2LCVDSX3_s1_2_IDT8_UDI_024_i7-IDT8_UDI_024_i5_6797-RN-0001.fastq.gz H2LCVDSX3_s1_2_IDT8_UDI_027_i7-IDT8_UDI_027_i5_6797-RN-0004.fastq.gz
+#run fastqc in parallel on each sequence file (forward and reverse)
+ls *.fastq.gz | parallel -j+0 --eta 'fastqc {}'
