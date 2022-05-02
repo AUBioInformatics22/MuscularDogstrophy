b1fd2f8bb062ea0829be87e2d4d0ee5252cf79f5
Cassidy Schneider <71412336+cns0045@users.noreply.github.com>
2022-05-01 23:47:20 -0500

    Update var_metrics.sh

--- scripts/var_metrics.sh
+++ scripts/var_metrics.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#source /opt/asn/etc/asn-bash-profiles-special/modules.sh
-#module load bcftools/1.13
-#module load tabix/2013-12-16
+source /opt/asn/etc/asn-bash-profiles-special/modules.sh
+module load bcftools/1.13
+module load tabix/2013-12-16
 
 ##### DIRECTORIES #####
 PROJDIR="/home/shared/stevison_group2"
@@ -17,68 +17,49 @@ cd "${VCFDIR}"
 
 mkdir -p stats
 
-#echo -e "Sample ID\tUnfiltered\tFiltered\tSelected\tSelectedBcftoolsStats\tSelectedBcftoolsStatsDMD\tIntersect\tIntersectBcftoolsStats\tIntersectDMD"
 echo -e "#sample\tunfiltered\tfiltered\tselected_chrX\tselected_DMD\tintersect_chrX\tintersect_DMD" >SNP_numbers.tsv
 
 for sample in "${sample_list[@]}"; do
+  #stats for sites that pass filtering
+  bcftools stats -f '.,PASS' "$sample.chrX.sorted.SNPs.filtered.vcf.gz" >"stats/$sample.chrX.filtered.filt.vcf_stats.txt"
+  
+  #stats for selected sites
+  bcftools stats "$sample.chrX.sorted.SNPs.filtered.select.filt.vcf.gz" >"stats/$sample.chrX.filtered.select.filt.vcf_stats.txt"
 
-#bcftools stats -f '.,PASS' "$sample.chrX.sorted.SNPs.filtered.vcf.gz" >"stats/$sample.chrX.filtered.filt.vcf_stats.txt"
+  #stats for selected sites of gene region
+  bcftools stats -r chrX:31264482-33330008 "$sample.chrX.sorted.SNPs.filtered.select.filt.vcf.gz" >"stats/$sample.chrX.filtered.select.filt.DMD.vcf_stats.txt"
 
-#bcftools stats "$sample.chrX.sorted.SNPs.filtered.select.filt.vcf.gz" >"stats/$sample.chrX.filtered.select.filt.vcf_stats.txt"
+  #unfiltered
+  u=$(zcat "$sample.chrX.sorted.SNPs.vcf.gz" | grep -v "^#" | wc -l)
 
-#stats for gene region of sample fsf
-bcftools stats -r chrX:31264482-33330008 "$sample.chrX.sorted.SNPs.filtered.select.filt.vcf.gz" >"stats/$sample.chrX.filtered.select.filt.DMD.vcf_stats.txt"
+  #filtered
+  ffb=$(grep -v "^#" "stats/$sample.chrX.filtered.filt.vcf_stats.txt" | awk '/number of SNPs/ {print $6}')
 
+  #selected
+  fsf=$(zcat "${sample}.chrX.sorted.SNPs.filtered.select.filt.vcf.gz" | grep -v "^#" | wc -l)
 
-u=$(zcat "$sample.chrX.sorted.SNPs.vcf.gz" | grep -v "^#" | wc -l)
+  #selected in DMD
+  fsfd=$(grep -v "^#" "stats/${sample}.chrX.filtered.select.filt.DMD.vcf_stats.txt" | awk '/number of SNPs/ {print $6}')
 
-ffb=$(grep -v "^#" "stats/$sample.chrX.filtered.filt.vcf_stats.txt" | awk '/number of SNPs/ {print $6}')
-
-fsf=$(zcat "${sample}.chrX.sorted.SNPs.filtered.select.filt.vcf.gz" | grep -v "^#" | wc -l)
-#fsfb=$(grep -v "^#" "stats/$sample.chrX.filtered.select.filt.vcf_stats.txt" | awk '/number of SNPs/ {print $6}')
-
-fsfd=$(grep -v "^#" "stats/${sample}.chrX.filtered.select.filt.DMD.vcf_stats.txt" | awk '/number of SNPs/ {print $6}')
-
-fsfidb=$(grep -v "^#" "stats/${sample}_isec_DMD.vcf_stats.txt" | awk '/number of SNPs/ {print $6}')
-fsficb=$(grep -v "^#" "stats/${sample}_isec_chrX.vcf_stats.txt" | awk '/number of SNPs/ {print $6}')
-
-
-printf "%s\t" "$sample" "$u" "$ffb" "$fsf" "$fsfd" "$fsficb" "$fsfidb" >>SNP_numbers.tsv
-printf "\n" >>SNP_numbers.tsv
+  #intersection in DMD
+  fsfidb=$(grep -v "^#" "stats/${sample}_isec_DMD.vcf_stats.txt" | awk '/number of SNPs/ {print $6}')
+  
+  #intersection in chrX
+  fsficb=$(grep -v "^#" "stats/${sample}_isec_chrX.vcf_stats.txt" | awk '/number of SNPs/ {print $6}')
+  
+  #print values for sample to output file
+  printf "%s\t" "$sample" "$u" "$ffb" "$fsf" "$fsfd" "$fsficb" "$fsfidb" >>SNP_numbers.tsv
+  printf "\n" >>SNP_numbers.tsv
 done
 
 cd intersect_SNP_chrX/
-#echo "Now in $(pwd)"
-
+#get number of common SNPs (chrX)
 all_fsficb=$(zcat all_samples_SNPs_chrX.vcf.gz | grep -v "^#" | wc -l)
 
 cd ../intersect_SNP_DMD/
-#echo "Now in $(pwd)"
-
+#get number of common SNPs (DMD)
 all_fsfidb=$(zcat all_samples_SNPs_DMD.vcf.gz | grep -v "^#" | wc -l)
 
 cd ..
-#echo "Now in $(pwd)"
-
+#print overall values to output file as a check
 printf "%s\t" "all" "---" "---" "---" "---" "$all_fsficb" "$all_fsfidb" >>SNP_numbers.tsv
-#echo
-
-
-#my_vcfs=()
-#for i in *SNPs.filtered.select.filt.vcf.gz; do
-#  my_vcfs+=(${i%.vcf.gz})
-#done
-
-#echo "VCF files: ${my_vcfs[@]}"
-#echo "Done. Now getting intersection of sites using vcf-compare"
-#bcftools stats "${my_vcfs[0]}.vcf.gz" "${my_vcfs[1]}.vcf.gz"
-#"${my_vcfs[2]}.vcf.gz" "${my_vcfs[3]}.vcf.gz" 
-#>intersection.4upsetR.txt
-
-#vcf-compare ${my_vcfs[0]}.vcf.gz ${my_vcfs[1]}.vcf.gz ${my_vcfs[2]}.vcf.gz ${my_vcfs[3]}.vcf.gz >intersection.4upsetR.txt
-#grep ^VN intersection.4upsetR.txt | cut -f 2- >intersection.4upsetR.venn
-
-#bcftools stats -r chrX:31264482-33330008 intersect_SNP/all_samples_SNPs.vcf.gz >stats/all_SNPs.DMD.vcf_stats.txt
-
-#echo "| Sample ID | Filtered | Selected | Intersect | bcftools stat |"
-#echo "|:---------:|:---------:|:---------:|:---------:|:---------:|"
